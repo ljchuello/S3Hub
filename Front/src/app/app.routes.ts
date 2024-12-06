7a29f6550b9f0e1b020ca5d1b50535e0b9b9404f
Leonardo Chuello <ljchuello@gmail.com>
2024-12-06 10:50:47 -0500

    Iniciando cosas!.

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {E404Component} from './shared/components/e404/e404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: '**',
+    component: E404Component,
+  }
+];
---
+++ Front/src/app/shared/components/e404/e404.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e404',
+  imports: [],
+  templateUrl: './e404.component.html',
+  styleUrl: './e404.component.css'
+})
+export class E404Component {
+
+}
